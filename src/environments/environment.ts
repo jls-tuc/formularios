f99c68a14b4701df461d92b9fd33b4f081cc0fa5
Jose Luis <65769995+jls-tuc@users.noreply.github.com>
2021-06-22 23:49:26 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,7 @@
 
 export const environment = {
   production: false,
-  //"http://10.1.142.117:3222",
-  //'http://localhost:8017',
-  //'https://0800covid.ciudadanianqn.gob.ar:3222',
-  //'http://localhost:8017',
-  apiUrl: 'http://localhost:3002',
-  //'https://0800covid.ciudadanianqn.gob.ar:3222',
-  //"http://192.168.24.197:8017",
-  //"http://181.209.23.177:8017",
+ 
 };
 
 /*
