c37f434c0c50c9b8e29b9303dd10e6a3f897adbc
Jose Luis <65769995+jls-tuc@users.noreply.github.com>
2021-06-22 23:52:45 -0300

    Update token-interceptor.service.ts

--- src/app/pages/services/token-interceptor.service.ts
+++ src/app/pages/services/token-interceptor.service.ts
@@ -11,7 +11,7 @@ export class TokenInterceptorService implements HttpInterceptor {
     let token = req.clone({
       setHeaders: {
         Authorization:
-          'jwt eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub21icmUiOiJmb3JtdWxhcmlvcyIsIm9yZ2FuaXphY2lvbiI6IjVmMTc0YjAwYjQ5MDUwNDZiOGZhYmU5ZSIsInR5cGUiOiJhcHAtdG9rZW4iLCJpYXQiOjE2MjEwOTU3MTYsImV4cCI6MTYzNjY0NzcxNn0.Tx-9PG86Y4FdQmLNty_r4RWfqyJO_tSyTUGR4kHmZRE',
+          '',
       },
     });
     return next.handle(token);
